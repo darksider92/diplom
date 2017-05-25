c16104b0de34223ff764f0a74a3171bfe299d087
Petr Kuznetsov <darksider92@mail.ru>
2017-05-25 10:20:12 +0600

    Оптимизация

--- diplom.py
+++ diplom.py
@@ -4,93 +4,88 @@ import time
 
 
 def get_params():
-    AUTHORIZE_URL = 'https://oauth.vk.com/authorize'
+    """Необходимые параметры для запроса."""
     VERSION = '5.64'
-    APP_ID = 5947968  # Your app_id here
-    access_token = ""  # token
+    access_token = "d13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981e1b4e296b14acb377e82dcbc81dc0f22"
     params = {'access_token': access_token,
               'v': VERSION,
               }
     return params
 
 
-def get_user_friends_id(params):
+def print_process():
+    """Показ процесса работы"""
     print('.')
-    response = requests.get('https://api.vk.com/method/friends.get', params)
-    friends_list = response.json()
-    return friends_list
 
 
-def get_user_groups(params):
-    print('.')
-    response = requests.get('https://api.vk.com/method/groups.get', params)
-    groups_list = response.json()
-    return groups_list
-
-
-def get_group_members_id(params):
-    print('.')
-    response = requests.get('https://api.vk.com/method/groups.getMembers', params)
-    members_list = response.json()
-    return members_list
+def make_request(url, params):
+    """Отправка запроса"""
+    print_process()
+    while True:
+        try:
+            response = requests.get(url, params)
+            response_list = response.json()
+            response_list['response']
+            break
+        except:
+            time.sleep(1)  # при возврате неверного ответа на запрос: ожидание 1с и отравка повторного запроса
+            # print('Except, a new try.')
+    return response_list
 
 
 def get_users_is_members(params, friends_list):
+    """Является ли пользователь из списка друзей подписчиком группы"""
     final_list = []
-    if len(params['user_ids'].split(', ')) > 250:
-        x = 0
-        y = 250
-        all_params = params['user_ids'].split(', ')
-        while all_params[x:y]:
-            params['user_ids'] = str(friends_list['response']['items'][x:y])[1:-1]
-            x = y
-            y += 250
-            # time.sleep(1)
-            print('.')
-            response = requests.get('https://api.vk.com/method/groups.isMember', params)
-            is_members = response.json()
-            for member in is_members['response']:
-                final_list.append(member)
+    x = 0
+    y = 250
+    all_params = params['user_ids'].split(', ')
+    while all_params[x:y]:
+        params['user_ids'] = str(friends_list['response']['items'][x:y])[1:-1]
+        x = y
+        y += 250
+        members = make_request('https://api.vk.com/method/groups.isMember', params)
+        for member in members['response']:
+            final_list.append(member)
     return final_list
 
 
 def get_group_without_user_friends(params, groups_list, friends_list):
+    """Является ли пользователь из списка друзей подписчиком группы"""
     user_is_along = []
     for group in groups_list['response']['items']:
         params['group_id'] = group['id']
         params['user_ids'] = str(friends_list['response']['items'])[1:-1]
         members_group = get_users_is_members(params, friends_list)
+        flag = False
         for member in members_group:
-            flag = False
             if member['member'] == 1:
                 flag = True
                 break
         if not flag:
             params['group_id'] = group['id']
-            try:
-                str_dict = {'name': group['name'],
-                            'gid': group['id'],
-                            'members_count': get_group_members_id(params)['response']['count']}
-            except:
-                print('except')
+            str_dict = {'name': group['name'], 'gid': group['id'],
+                        'members_count': make_request('https://api.vk.com/method/groups.getMembers',
+                                                      params)['response']['count']}
             user_is_along.append(str_dict)
-        time.sleep(1)
     return user_is_along
 
 
 def save_json(data):
+    """Сохранение файла в json"""
     with open('result.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+        print(len(data))
 
 
 def main():
     params = get_params()
     params['user_id'] = 5030613
-    friends_list = get_user_friends_id(params)
+    friends_list = make_request('https://api.vk.com/method/friends.get', params)
     params['extended'] = 1
-    groups_list = get_user_groups(params)
-
+    groups_list = make_request('https://api.vk.com/method/groups.get', params)
     data = get_group_without_user_friends(params, groups_list, friends_list)
     save_json(data)
 
-main()
+
+if __name__ == '__main__':
+    main()
