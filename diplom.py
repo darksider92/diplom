c89704d002df8c61eee50fef3fe80e77d35eaefb
PetrKuznetsov <darksider92@mail.ru>
2017-05-29 20:20:51 +0600

    Откат

--- diplom.py
+++ diplom.py
@@ -3,16 +3,6 @@ import json
 import time
 
 
-def get_params():
-    """Необходимые параметры для запроса."""
-    VERSION = '5.64'
-    access_token = "d13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981e1b4e296b14acb377e82dcbc81dc0f22"
-    params = {'access_token': access_token,
-              'v': VERSION,
-              }
-    return params
-
-
 def print_process():
     """Показ процесса работы"""
     print('.')
